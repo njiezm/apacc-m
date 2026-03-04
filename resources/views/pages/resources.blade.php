0dfe313a03c071f5c34fcf0ad5c446c31377e1f5
Njie <njie@wak-studio.com>
2026-03-04 17:14:28 -0400

    ressource

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -488,6 +488,7 @@
                         </div>
                         <div class="col-md-4">
                             <img src="{{ asset('images/histoire/vitrail-1.jpg') }}" class="img-fluid rounded mb-3" alt="Vitrail">
+                            <p>Vitrail de l'église de St-Christophe</p>
                             {{-- <img src="{{ asset('images/histoire/vitrail-2.jpg') }}" class="img-fluid rounded" alt="Vitrail détail"> --}}
                         </div>
                     </div>
