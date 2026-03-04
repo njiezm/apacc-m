832c7907318b71d65fdf6fba903f08ad46a1dee3
Njie <njie@wak-studio.com>
2026-03-04 17:13:12 -0400

    ressource

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -488,7 +488,7 @@
                         </div>
                         <div class="col-md-4">
                             <img src="{{ asset('images/histoire/vitrail-1.jpg') }}" class="img-fluid rounded mb-3" alt="Vitrail">
-                            <img src="{{ asset('images/histoire/vitrail-2.jpg') }}" class="img-fluid rounded" alt="Vitrail détail">
+                            {{-- <img src="{{ asset('images/histoire/vitrail-2.jpg') }}" class="img-fluid rounded" alt="Vitrail détail"> --}}
                         </div>
                     </div>
                 </div>
