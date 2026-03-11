e267783a5165f7d0e3c662106a76c82f5d250e9c
Njie <njie@wak-studio.com>
2026-03-10 21:42:34 -0400

    ajouts

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -334,7 +334,7 @@ Nos formations : cycle de formation “géométrie sacrée”, guide du patrimoi
                   <div class="modal-body text-center">
                     {{-- Ici, image différente et plus petite --}}
                     <img src="{{ asset('images/actions/modal/' . $action['img'] . '-small.jpg') }}" 
-                         class="mx-auto mb-4 rounded max-w-[300px] h-auto">
+                         class="mx-auto mb-4 rounded max-w-[450px] h-auto">
 
                     <p class="text-gray-700 text-left whitespace-pre-line">
                         {{ $action['description'] ?? 'Description détaillée à ajouter pour ce bloc.' }}
